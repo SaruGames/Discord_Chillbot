8c3ab0dff27f0aaf071e80ece03f53903f9e40c8
SaruGames <98968432+SaruGames@users.noreply.github.com>
2022-06-12 14:09:02 +0200

    Update chillbot.js

--- chillbot.js
+++ chillbot.js
@@ -1,12 +1,36 @@
-const Discord = require('discord.js');
-const client = Discord.Client();
+const express = require("express");
+const app = express();
+const discord = require("discord.js");
+const client = new discord.Client({intents:["GUILDS","GUILD_MESSAGES"]});
+var bump_timeout = true;
+app.listen(3000,()=>{
+  console.log("bot is online");
+});
 
-client.on('ready', () =>{
-  console.log("Im online again!")
+app.get("/",(req, res) =>{
+  res.send("hello world uwu");
+});
+client.on("message", message => {
+  var msg_value = message.content.toLowerCase();
+  var username = message.author.username;
+  var id = message.author.id;
+  if(msg_value.includes('hello') && msg_value.includes('bot') || msg_value.includes('hi') && msg_value.includes('bot')){
+    message.channel.send("hi dad");
+  }
+  if(message.content === "!d bump" && bump_timeout==true){
+    bump_timeout = false;
+    message.channel.send("got 100 coins!");
+    setTimeout(bump_switchtimeout, 5000);
+  }
 })
-client.on('message', msg=>{
-  if(msg.content === "!test"){
-    msg.reply('1234 test 1234 working beep boop')
-   }
-  )
-client.login(process.env.BOT_TOKEN);
+
+function bump_switchtimeout(){
+  bump_timeout = true;
+}
+
+client.on("ready", () => {
+  console.log("bot is ready");
+})
+
+
+client.login("OTg1MTk2NDIyNzk2Mjc5ODU4.GNso_s.clK6uJLKPFK3AV9FeYNA8HOCB8iR0vEcYIdI-8");
